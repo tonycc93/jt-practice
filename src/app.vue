2e98443c5d82b162feaa6bb801945407a72e5825
江涛 <tao.jiang02@hand-china.com>
2019-03-11 16:16:45 +0800

    初始化

---
+++ src/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">{{text}}</div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                text: 'for test/how can I do for you'
+            }
+        }
+    }
+</script>
+<style></style>
